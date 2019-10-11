4f99445602fcb56cdc6bfa5867d086d99e7cb500
Liam <lon.liam.li@gmail.com>
2019-10-10 21:53:46 -0700

    navbar started

--- app/backend/client/src/App.js
+++ app/backend/client/src/App.js
@@ -1,33 +1,13 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import UserTest from  './components/user/user_test';
 import './App.css';
 
-class App extends Component {
-  state = {
-    users:[]
-  }
-  componentDidMount(){
-    this.getUsers();
-  }
-
-  getUsers = _ => {
-        axios.get('/User')
-        .then((data) => {
-      console.log(data.data.User);
-      this.setState({users: data.data.User});
-    })
-    // .then(({response}) => this.setState({users: response.users}))
-    .catch(error => console.log(error));
-  }
-  showUsers = user => <div key={user.User_id}>{user.User_name}</div>
-  render() {//building react method that comes inse od react component
-    const { users } = this.state;
-    return (//jsx code and can return only a single parent tag
-      <div className="App">
-        {users.map(this.showUsers)}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <UserTest /> 
+    </div>
+  );
 }
 
 export default App;
